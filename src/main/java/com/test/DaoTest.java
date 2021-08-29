e853716ba5e29cf59e879c690e90100efd391ed7
lijuly <july120681@163.com>
2021-08-29 16:36:34 +0800

    初始提交

--- src/main/java/com/test/DaoTest.java
+++ src/main/java/com/test/DaoTest.java
@@ -1,5 +1,32 @@
 package com.test;
 
+import com.media.dao.IAccountDao;
+import com.media.entity.Account;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 public class DaoTest {
+
+    @Test
+    public void findAllTest(){
+        try (InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml")){
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+            try (SqlSession session= factory.openSession()) {
+                IAccountDao aDao = session.getMapper(IAccountDao.class);
+                List<Account> list = aDao.findAll();
+                System.out.println(list);
+            }
+        } catch (IOException e) {
+
+        }
+
+    }
 
 }
